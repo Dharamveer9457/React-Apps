b49bab4d5dd598ad4e5ae7c9feb9211985e8d526
Dharamveer <dharammveer945734@outlook.com>
2023-11-24 12:42:46 +0530

    Contest setup

---
+++ whatsapp-clone/frontend/talktrek/src/Components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../assets/Logo.png'
+
+function main() {
+  return (
+    <div className='MainBox'>
+      <div className='left-box'>
+        <img src={logo} alt="" />
+      </div>
+      <div className='right-box'>
+
+      </div>
+    </div>
+  )
+}
+
+export default main
